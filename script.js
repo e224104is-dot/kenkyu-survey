71a2449b3b5dc9caa12df0c9ec3c6cdfea26e1d3
e224104is-dot <e224104is@kurume-it.ac.jp>
2025-12-19 05:22:52 +0900

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const ENDPOINT_URL =
-  "https://script.google.com/macros/s/AKfycbzNfU8hBeIJCrZFIrEICsr7uaEOx5hnRfdU0hzeYbcfYV3_C07yLooXHVj3NGF_gALZ/exec";
+  "https://script.google.com/macros/s/AKfycbzWzc5NKPsa6ORwaHOquCDtgc6Iq518ixK_PK4hPhNoSVYqTGYMvVTaGytSYeasKdfQ/exec";
 
 const questions = [
   { text: "外向的で、社交的である", trait: "E", reverse: false },
